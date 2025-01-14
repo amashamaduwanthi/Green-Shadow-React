a8b289d38d1548579b6a79e3104922f9f38d7faa
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-01-14 19:06:20 +0400

    delete crop complete

--- src/pages/crop.tsx
+++ src/pages/crop.tsx
@@ -1,6 +1,22 @@
 import {Link} from "react-router";
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {deleteCrop} from "../redux/slices/cropSlice.ts";
 
 export function Crop(){
+    const crop = useSelector((state:any)=>state.crop);
+    const dispatch = useDispatch();
+    const [deleteCropCode, setDeleteCropCode] = useState('');
+    function handleDeleteCrop(event: React.FormEvent) {
+        event.preventDefault();
+        if(!deleteCropCode){
+            alert("Crop Code is empty");
+
+        }
+        dispatch(deleteCrop(deleteCropCode));
+        setDeleteCropCode('');
+    }
+
     return (
         <>
             <br/>
@@ -9,7 +25,20 @@ export function Crop(){
                 <Link to="/crop/Add">
                     <button>Add Crop</button>
                 </Link>
+
+                <input type="text" placeholder="enter the crop code" value={deleteCropCode}
+                       onChange={(e) => setDeleteCropCode(e.target.value)}/>
+
+                <button onClick={handleDeleteCrop}>Delete Crop</button>
+                <br/>
+                <ul>
+                    {crop.map((cropDetails: any, index: number) => (
+                        <li key={index}>
+                            {cropDetails.cropId}, {cropDetails.cropName},{cropDetails.scientificName},{cropDetails.image},{cropDetails.category},{cropDetails.season},{cropDetails.fieldCode}
+                        </li>
+                    ))}
+                </ul>
             </div>
-            </>
-            )
-            }
+        </>
+    )
+}
